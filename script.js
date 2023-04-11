7720726435d593931f099cda9e920f14417803e4
Agnieszka Ksiazek <agnieszka.dubikowska@gmail.com>
2023-04-11 23:16:48 +0200

    searchElement function added

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let elements = []
 const addElement = function (newElement) {
    elements = elements.concat(newElement)
 }
+
+const searchElement = function (element) {
+    return elements.includes(element)
+}
+

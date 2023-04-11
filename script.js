0f343358f4b0d8b0affe5d4fe9b18c6519659a01
Agnieszka Ksiazek <agnieszka.dubikowska@gmail.com>
2023-04-11 23:38:16 +0200

    render and init functions added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let elements = []
+let elements = [bromine, cadmium, lead]
 
 const addElement = function (newElement) {
    elements = elements.concat(newElement)
@@ -6,5 +6,38 @@ const addElement = function (newElement) {
 
 const searchElement = function (element) {
     return elements.includes(element)
+}
+
+const renderList = function(){}
+const renderNewElementInput = function(){}
+const renderSearchInput = function(){}
+const renderSearchResult = function(){}
+
+
+const render = function () {
+
+    const div = document.createElement('div')
+
+    const list = renderList
+    const newElementInput = renderNewElementInput
+    const searchInput = renderSearchInput
+    const searchResult = renderSearchResult
+
+    div.appendChild(list)
+    div.appendChild(newElementInput)
+    div.appendChild(searchInput)
+    div.appendChild(searchResult)
+
+    return div
+}
+
+const init = function (containerSelector) {
+    const container = document.querySelector(containerSelector)
+    
+    if(!container) render
+    
+    const app = render()
+
+    container.appendChild(app)
 }
 
